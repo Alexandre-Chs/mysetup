e469e2383b0a1de3b6223ee8064c1c6b776709f5
Alexandre-Chs <alexandrechanas0@gmail.com>
2024-06-01 09:40:09 +0200

    feat: ✨ Add validate requests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Feed from "@/components/feed/Feed";
+import { validateRequest } from "@/lib/auth/validate-request";
 
-export default function Home() {
+export default async function Home() {
+  const { user } = await validateRequest();
+  console.log(user);
+  if (!user) {
+    <div>Not connected !</div>;
+  }
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1>Hello, {user?.username}!</h1>
       <Feed />
     </div>
   );
