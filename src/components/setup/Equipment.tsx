1500b243472cf5e192dfc67fbaa9e0adedaf235d
Alexandre Chs <oliuuum@gmail.com>
2024-06-29 11:32:24 +0200

    💄 Fix scrollbar style

--- src/components/setup/Equipment.tsx
+++ src/components/setup/Equipment.tsx
@@ -22,27 +22,29 @@ const Equipment = () => {
   const groupedItems = groupByType(FAKES);
 
   return (
-    <div className="h-full bg-[#151515] text-white px-4 rounded-large overflow-y-scroll scrollbar">
-      {Object.keys(groupedItems).map((type) => (
-        <div key={type}>
-          <h4 className="font-bold text-2xl pt-4 pb-2 capitalize">{type}</h4>
-          {groupedItems[type].map(
-            (item: { name: string; type: string }, index: number) => (
-              <div
-                className="w-full bg-[#464646] rounded-md flex items-center justify-start gap-2 py-2 px-4 mb-4"
-                key={index}
-              >
-                {item.type === "equipment" ? (
-                  <IoHardwareChip className="text-orange-400" size={20} />
-                ) : (
-                  <LuLampDesk className="text-blue-400" size={20} />
-                )}
-                <p>{item.name}</p>
-              </div>
-            )
-          )}
-        </div>
-      ))}
+    <div className="overflow-hidden h-full rounded-large">
+      <div className="h-full bg-[#151515] text-white px-4 rounded-large overflow-y-auto scrollbar">
+        {Object.keys(groupedItems).map((type) => (
+          <div key={type}>
+            <h4 className="font-bold text-2xl pt-4 pb-2 capitalize">{type}</h4>
+            {groupedItems[type].map(
+              (item: { name: string; type: string }, index: number) => (
+                <div
+                  className="w-full bg-[#464646] rounded-md flex items-center justify-start gap-2 py-2 px-4 mb-4"
+                  key={index}
+                >
+                  {item.type === "equipment" ? (
+                    <IoHardwareChip className="text-orange-400" size={20} />
+                  ) : (
+                    <LuLampDesk className="text-blue-400" size={20} />
+                  )}
+                  <p>{item.name}</p>
+                </div>
+              )
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
