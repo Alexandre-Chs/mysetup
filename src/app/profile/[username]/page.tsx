d4823398f00067c73356b3c8be58adb5afbb0687
Alexandre Chs <oliuuum@gmail.com>
2024-07-17 17:09:07 +0200

    revert

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -18,10 +18,13 @@ export default async function Page({
     notFound();
   }
 
+  const capitalizedUsername =
+    username.charAt(0).toUpperCase() + username.slice(1);
+
   return (
     <div className="flex flex-col h-[90vh] max-w-[1000px] mx-auto">
-      <h1 className="font-bold text-3xl my-4 capitalize">
-        {username}&apos;s setups
+      <h1 className="font-bold text-3xl my-4">
+        {capitalizedUsername}&apos;s setups
       </h1>
       {setups.map((setup) => (
         <a href={`/setup/${setup.id}`} key={setup.id}>
