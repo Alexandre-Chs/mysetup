a972579ad614af42e4cfe497238269b1b616b1a0
unkobweb <sieg.alexandre@gmail.com>
2024-07-27 10:56:02 +0200

    better carousel

--- src/components/setup/carousel/Carousel.tsx
+++ src/components/setup/carousel/Carousel.tsx
@@ -40,64 +40,61 @@ const Carousel: React.FC<PropType> = (props) => {
   } = usePrevNextButtons(emblaApi);
 
   return (
-    <div className="flex flex-row items-center gap-8 h-full w-full">
-      <div className="bg-white rounded-full flex items-center justify-center">
-        <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-      </div>
-      
-      <div className="flex-1 flex flex-row">
-        <section className="embla !max-w-none !m-0">
-          <div className="embla__viewport h-full w-full flex items-center" ref={emblaRef}>
-            <div className="embla__container flex items-center">
-              {slides.map((slide) => (
-                <div className="embla__slide !m-0 w-fit" key={slide.id}>
-                  <div className="flex flex-row w-fit">
+    <div className="flex flex-row h-full w-full p-8 gap-4">
+      <div className="flex flex-col w-5/6 gap-4">
+        <div className="h-5/6 flex items-center">
+          <section className="embla">
+            <div className="embla__viewport" ref={emblaRef}>
+              <div className="embla__container">
+                {slides.map((slide) => (
+                  <div className="embla__slide flex items-center" key={slide.id}>
                     <img
                       src={slide.media.url}
-                      className="rounded-xl max-h-[50%] max-w-[50%]"
+                      className="rounded-xl m-auto max-w-full max-h-full"
                       alt=""
                     />
-                    <div className="">
-                      <Equipment
-                        equipments={slides[selectedIndex].photoEquipments.map(
-                          (pE: any) => pE.equipment
-                        )}
-                      />
-                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </div>
-
-          <div className="embla__controls">
-            <div className="embla__buttons"></div>
+          </section>
+        </div>
+        <div className="h-1/6 w-full">
+          <div className="embla__controls !m-0">
+            <div className="embla__buttons">
+              <div className="bg-white rounded-xl p-1">
+                <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+              </div>
+              <div className="bg-white rounded-xl p-1">
+                <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+              </div>
+            </div>
 
             <div className="embla__dots">
               {scrollSnaps.map((_, index) => (
                 <DotButton
                   key={index}
                   onClick={() => onDotButtonClick(index)}
-                  className={"embla__dot".concat(
-                    index === selectedIndex ? " embla__dot--selected" : ""
+                  className={'embla__dot'.concat(
+                    index === selectedIndex ? ' embla__dot--selected' : ''
                   )}
                 />
               ))}
             </div>
           </div>
-        </section>
-        {/* <div className="h-[78vh] w-full self-center">
-          {slides.length > 0 && (
-
-          )}
-        </div> */}
+        </div>
       </div>
-
-      <div className="bg-white flex items-center justify-center rounded-full">
-        <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+      <div className="w-1/6 h-full">
+        <Equipment
+          equipments={slides[selectedIndex].photoEquipments.map(
+            (pE: any) => pE.equipment
+          )}
+        />
       </div>
     </div>
   );
 };
 
 export default Carousel;
+
+
