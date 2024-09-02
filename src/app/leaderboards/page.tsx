d2ead58e9c972b0386fd0c6b5d8aedd424a23b44
Alexandre Chs <oliuuum@gmail.com>
2024-09-02 20:08:11 +0200

    le poulet est délicieux

--- src/app/leaderboards/page.tsx
+++ src/app/leaderboards/page.tsx
@@ -4,23 +4,23 @@ import React from "react";
 
 const Leaderboards = () => {
   return (
-    <div className="min-h-[90vh] w-full rounded-md flex md:items-center md:justify-center antialiased relative px-8 md:px-16 lg:px-24">
+    <div className="min-h-[90vh] w-full rounded-md flex md:items-center md:justify-center antialiased relative md:px-16 lg:px-24 pb-12">
       <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="#E6E6E6"
+        className="-top-40 left-0 md:left-[23rem] xl:left-[15rem]"
+        fill="#e6e6e6a9"
       />
       <div className="w-full flex flex-col items-center justify-center">
         <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
           <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
             Leaderboards
           </h1>
-          <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+          <p className="mt-4 font-normal text-base text-neutral-300 max-w-[18rem] sm:max-w-lg text-center mx-auto">
             Each month we&apos;ll be highlighting the best setup of the month.{" "}
-            <br /> The first winner will be the one with the most upvotes.{" "}
-            <br /> He win a gift card.
+            <br /> The winner will be the one with the most upvotes. <br /> He
+            win a gift card.
           </p>
         </div>
-        <div className="mt-8 w-full">
+        <div className="mt-20 w-full">
           <LeaderboardTable />
         </div>
       </div>
--- src/components/leaderboards/LeaderboardTable.tsx
+++ src/components/leaderboards/LeaderboardTable.tsx
@@ -1,46 +1,119 @@
+import clsx from "clsx";
+import { Link2 } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const LeaderboardTable = () => {
   const items = [
-    { username: "You", setup: "0.00", upvotes: 0 },
-    { username: "marion_stiedemann", setup: "2.00", upvotes: 5 },
-    { username: "shannon_kautzer", setup: "2.00", upvotes: 5 },
-    { username: "arthur_grimes", setup: "2.00", upvotes: 5 },
-    { username: "bernadette_mclaughlin", setup: "2.00", upvotes: 5 },
-    { username: "alberta_spencer", setup: "2.00", upvotes: 5 },
-    { username: "leo_ruecker", setup: "2.00", upvotes: 5 },
-    { username: "rudolph_boehm", setup: "2.00", upvotes: 5 },
+    {
+      title: "My ultimate gaming setup with RTX 4090",
+      username: "amelia_brown",
+      setup: "#",
+      upvotes: 12,
+    },
+    {
+      title: "Artistic workstation setup with dual monitors",
+      username: "josephine_ortiz",
+      setup: "#",
+      upvotes: 8,
+    },
+    {
+      title: "Summer setup with RTX 3070 and LED lights",
+      username: "liam_jones",
+      setup: "#",
+      upvotes: 15,
+    },
+    {
+      title: "Minimalist office setup with standing desk",
+      username: "olivia_martinez",
+      setup: "#",
+      upvotes: 20,
+    },
+    {
+      title: "Cozy winter setup with heated chair and 3080",
+      username: "noah_smith",
+      setup: "#",
+      upvotes: 18,
+    },
+    {
+      title: "RGB heaven with water-cooled PC",
+      username: "sophia_taylor",
+      setup: "#",
+      upvotes: 25,
+    },
+    {
+      title: "Home theater setup with 4K projector",
+      username: "lucas_johnson",
+      setup: "#",
+      upvotes: 22,
+    },
+    {
+      title: "Portable setup for digital nomads",
+      username: "mia_wilson",
+      setup: "#",
+      upvotes: 10,
+    },
+    {
+      title: "High-end audio setup with surround sound",
+      username: "ethan_davis",
+      setup: "#",
+      upvotes: 30,
+    },
+    {
+      title: "Sleek workstation with ultra-wide monitor",
+      username: "isabella_moore",
+      setup: "#",
+      upvotes: 17,
+    },
   ];
 
   return (
-    <table className="w-full border border-[#212124]">
+    <table className="w-full border border-[#212124] max-w-6xl mx-auto">
       <thead>
         <tr>
-          <th className="px-4 py-2 text-left text-textColor border-[1px] text-center border-[#212124]">
+          <th className="px-4 py-2 text-textColor border-[1px] text-center border-[#212124]">
             Rank
           </th>
-          <th className="px-4 py-2 text-left text-textColor border-[1px] text-center border-[#212124]">
-            Player
+          <th className="px-4 py-2 text-textColor border-[1px] text-center border-[#212124]">
+            Username
           </th>
-          <th className="px-4 py-2 text-left text-textColor border-[1px] text-center border-[#212124]">
-            Won
+          <th className="px-4 py-2 text-textColor border-[1px] text-center border-[#212124]">
+            Setup
           </th>
-          <th className="px-4 py-2 text-left text-textColor border-[1px] text-center border-[#212124]">
-            Trades
+          <th className="px-4 py-2 text-textColor border-[1px] text-center border-[#212124]">
+            Upvotes
           </th>
         </tr>
       </thead>
       <tbody>
         {items.map((item, index) => (
           <tr key={index} className="border-b border-[#212124]">
-            <td className="px-4 py-2 text-colorTextLighter border-[1px] border-[#212124] text-center">
-              {index + 1}
+            <td
+              className={clsx(
+                "relative px-4 py-2 text-colorTextLighter border-[1px] border-[#212124] text-center",
+                index === 0 && "font-bold text-redText"
+              )}
+            >
+              [ {index + 1} ]
+              {index === 0 && (
+                <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-6 h-6 bg-redText/35 rounded-full blur-xl"></div>
+              )}
             </td>
             <td className="px-4 py-2 text-center text-colorTextLighter border-[1px] border-[#212124]">
               {item.username}
             </td>
-            <td className="px-4 py-2 text-colorTextLighter border-[1px] border-[#212124] text-center">
-              {item.setup}
+            <td className="px-4 py-2 text-colorTextLighter border-[#212124] text-center flex items-center justify-center">
+              <Link
+                href={`/${item.setup}`}
+                className="text-colorTextLighter hidden md:block"
+              >
+                {item.title}
+              </Link>
+              <div className="block md:hidden cursor-pointer">
+                <Link href={`/${item.setup}`}>
+                  <Link2 />
+                </Link>
+              </div>
             </td>
             <td className="px-4 py-2 text-colorTextLighter border-[1px] border-[#212124] text-center">
               {item.upvotes}
