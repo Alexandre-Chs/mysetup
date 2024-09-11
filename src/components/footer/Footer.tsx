9794349b99ef2586f259ca10219ba1da097064d3
Alexandre Chs <oliuuum@gmail.com>
2024-09-11 17:55:49 +0200

    wip footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+  //TODO: faire un bg transparent avec une lumiere blanche derriere qui ressort un peu pour faire zoulie
+  return (
+    <div className="bg-red-500 w-full h-40 text-white text-center">Footer</div>
+  );
+};
+
+export default Footer;
