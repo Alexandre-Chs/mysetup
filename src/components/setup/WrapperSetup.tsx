07f605a10a1571b158c793d0bdf49b3f9ba9d8d9
Alexandre Chs <oliuuum@gmail.com>
2024-09-22 10:26:46 +0200

    add social links on bento username

--- src/components/setup/WrapperSetup.tsx
+++ src/components/setup/WrapperSetup.tsx
@@ -5,6 +5,9 @@ import WrapperDescriptionSetup from "./WrapperDescriptionSetup";
 import { User } from "@/types/types";
 import WrapperEquipmentSetup from "./WrapperEquipmentSetup";
 import { useSetupStore } from "@/store/SetupStore";
+import { useQuery } from "@tanstack/react-query";
+import { getUserInfos } from "@/actions/user/getInfos";
+import { getSocialIcon } from "@/lib/utils/show-social-icons";
 
 const WrapperSetup = ({
   currentUser,
@@ -14,7 +17,15 @@ const WrapperSetup = ({
   isOwner: boolean;
 }) => {
   const setup = useSetupStore((state) => state.setup);
-  if (!setup) return null;
+  const { data: userInfos } = useQuery({
+    queryKey: ["userInfos", currentUser?.username],
+    queryFn: async () => {
+      if (!currentUser) return null;
+      return await getUserInfos(currentUser.username);
+    },
+  });
+
+  if (!setup) return;
 
   return (
     <div className="h-[900px] w-full grid grid-cols-4 grid-rows-8 gap-6">
@@ -25,8 +36,15 @@ const WrapperSetup = ({
         <WrapperEquipmentSetup setupId={setup?.id} isOwner={isOwner} />
       </div>
       <div className="col-span-1 row-span-1 row-start-7">
-        <div className="h-full bento-smooth-gradient border-[1px] border-[#1F2022] text-white flex items-center justify-center gap-4 px-4 rounded-large cursor-pointer">
-          <p>alexandre</p>
+        <div className="h-full flex-col bento-smooth-gradient border-[1px] border-[#1F2022] text-white flex items-center justify-center gap-4 px-4 rounded-large">
+          <p className="font-medium">{currentUser?.username}</p>
+          <div className="flex gap-x-4">
+            {userInfos?.socialLinks.map((link: any) => (
+              <a key={link.id} href={link.link} target="_blank">
+                {getSocialIcon(link.socialName)}
+              </a>
+            ))}
+          </div>
         </div>
       </div>
       <div className="col-span-1 row-span-1 row-start-8">
