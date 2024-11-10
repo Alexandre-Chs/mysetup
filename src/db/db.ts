da6cb35720e1fef00048c81e78f26344213dd99b
Alexandre Chs <oliuuum@gmail.com>
2024-11-10 11:17:33 +0100

    change pw

--- src/db/db.ts
+++ src/db/db.ts
@@ -20,7 +20,7 @@ export const pool = new Pool({
   host: process.env.DB_HOST || "127.0.0.1",
   port: +(process.env.DB_PORT || 5432),
   user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASS || "root",
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME || "mysetup",
 });
 
