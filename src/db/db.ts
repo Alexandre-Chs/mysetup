632a16240ad16d9f2aee252259b216c7201be1f7
Alexandre Chs <oliuuum@gmail.com>
2024-11-10 10:52:01 +0100

    add log

--- src/db/db.ts
+++ src/db/db.ts
@@ -6,15 +6,15 @@ declare global {
   var drizzle: NodePgDatabase<typeof schema> | undefined;
 }
 
-console.log("DATABAAAAAAAAAASE")
+console.log("DATABAAAAAAAAAASE");
+console.log("DB pass", process.env.DB_PASS);
 console.log({
   host: process.env.DB_HOST || "127.0.0.1",
   port: +(process.env.DB_PORT || 5432),
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASS || "root",
   database: process.env.DB_NAME || "mysetup",
-})
-
+});
 
 export const pool = new Pool({
   host: process.env.DB_HOST || "127.0.0.1",
