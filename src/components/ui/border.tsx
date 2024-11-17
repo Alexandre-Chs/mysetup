ffe8335defb8746a408305b80ee49aa6e8bc532f
Alexandre Chs <oliuuum@gmail.com>
2024-11-17 08:46:05 +0100

    fix colors

--- src/components/ui/border.tsx
+++ src/components/ui/border.tsx
@@ -3,10 +3,8 @@ import React, { PropsWithChildren } from "react";
 const Border = ({ children }: PropsWithChildren<{}>) => {
   return (
     <div className="relative h-full w-full">
-      <div className="absolute -inset-2 h-full rounded-lg bg-gradient-to-b from-gray-400/35 via-gray-600/35 to-gray-800/35 opacity-50 blur-2xl"></div>
-      <div className="relative h-full border border-zinc-700/50 rounded-lg bg-zinc-900/50 text-slate-300">
-        {children}
-      </div>
+      <div className="absolute -inset-2 h-full rounded-lg bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-gray-900 via-gray-600 to-gray-600 opacity-25 blur-2xl"></div>
+      <div className="relative h-full border border-zinc-900 rounded-lg bg-zinc-950 text-slate-300">{children}</div>
     </div>
   );
 };
