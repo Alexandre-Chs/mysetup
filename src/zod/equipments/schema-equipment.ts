1e5a3520681ac4d7c3f6a5996bf4eab6edd0bf24
Alexandre Chs <oliuuum@gmail.com>
2024-11-30 08:18:23 +0100

    update regex on equipments

--- src/zod/equipments/schema-equipment.ts
+++ src/zod/equipments/schema-equipment.ts
@@ -5,7 +5,7 @@ export const validSchemaEquipment = z.object({
     .string()
     .min(1, { message: "You must enter a name" })
     .max(31, { message: "Name must be at most 31 characters long" })
-    .regex(/^[a-zA-Z0-9-_ ]+$/, {
+    .regex(/^[a-zA-Z0-9-_ '""]+$/, {
       message: "Name can only contain letters, numbers, dashes and underscores",
     }),
   category: z.enum(["Main Hardware", "Audio", "Desk Setup", "Lighting", "Accessories", "Decoration", "Components"], {
